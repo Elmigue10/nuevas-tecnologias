65e8977db525edb8cc7ca7511c184d532f5b36c6
Elmigue10 <miguelvalbuena1026@gmail.com>
2022-11-19 14:28:01 -0500

    Guia 9

---
+++ Guia9/spring-aws-lambda-rest-api/src/main/java/dev/miguel/springawslambdarestapi/SpringAwsLambdaRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.miguel.springawslambdarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsLambdaRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsLambdaRestApiApplication.class, args);
+    }
+
+}
---
+++ Guia9/spring-aws-lambda-rest-api/src/test/java/dev/miguel/springawslambdarestapi/SpringAwsLambdaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.miguel.springawslambdarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsLambdaRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
